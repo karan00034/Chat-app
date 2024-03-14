01e5704aab08b67be163746117a989c5bddfa5f2
Karan Bhardwaj <91610499+karan00034@users.noreply.github.com>
2024-03-15 02:07:26 +0530

    Update Chat.jsx

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -22,7 +22,7 @@ export default function Chat(){
     },[selectedUserId]);
 
     function connectToWs(){
-        const ws=new WebSocket('wss://chat-app-eight-ochre.vercel.app/');
+        const ws=new WebSocket('wss://chat-app-eight-ochre.vercel.app');
         setWs(ws);
         ws.addEventListener('message',handleMessage);
         ws.addEventListener('close',()=>{
